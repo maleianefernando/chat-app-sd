5e90dee64711236a41a1b17c79c10fa6eee240b3
Fernando Maleiane <maleianefernando3@gmail.com>
2025-04-26 23:03:26 +0200

    feat: run databse migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->unsignedBigInteger('phone')->unique();
+            $table->string('username');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique();
+            $table->timestamp('online_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
