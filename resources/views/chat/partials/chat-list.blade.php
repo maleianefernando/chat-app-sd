28b506f1a593034e853ee9030ee0a85e97123df3
Fernando Maleiane <maleianefernando3@gmail.com>
2025-05-04 22:56:06 +0200

    show the chat names

--- resources/views/chat/partials/chat-list.blade.php
+++ resources/views/chat/partials/chat-list.blade.php
@@ -2,31 +2,8 @@
 @foreach ($chats as $chat)
 <div class="chat-item user-item"><a href="{{ route('chat.specific', $chat->id) }}" class="chat-item-link"><img
     src="https://png.pngtree.com/png-vector/20190420/ourmid/pngtree-vector-business-man-icon-png-image_966609.jpg"
-    class="rounded-circle me-2" alt="User" /><span>{{ $chat->is_group ? $chat->name : ( ($other_side_user->username == null) ? $other_side_user->phone : $other_side_user->username ) }}</span></a>
+    class="rounded-circle me-2" alt="User" /><span>{{ $chat_names["".$chat->id] }}</span></a>
 </div>
 @endforeach
 
-{{-- @if (isset($user))
-<div class="chat-item user-item"><a href="{{ route('chat.specific', 1) }}" class="chat-item-link"><img
-    src="https://img.freepik.com/free-photo/no-idea_273609-23908.jpg?t=st=1744608982~exp=1744612582~hmac=9f2c831411c907be117bb07d9330499b08afe231d4fc7c1354ea7c0a3c1c98db&w=996"
-    class="rounded-circle me-2" alt="User" /><span>{{ $user }}</span></a>
-</div>
-@elseif (isset($group))
-<div class="chat-item user-item"><a href="{{ route('chat.specific', 1) }}" class="chat-item-link"><img
-    src="https://img.freepik.com/free-photo/no-idea_273609-23908.jpg?t=st=1744608982~exp=1744612582~hmac=9f2c831411c907be117bb07d9330499b08afe231d4fc7c1354ea7c0a3c1c98db&w=996"
-    class="rounded-circle me-2" alt="User" /><span>{{ $group }}</span></a>
-</div>
-@endif
-<div class="chat-item user-item"><a href="{{ route('chat.specific', 1) }}" class="chat-item-link"><img
-    src="https://img.freepik.com/free-photo/no-idea_273609-23908.jpg?t=st=1744608982~exp=1744612582~hmac=9f2c831411c907be117bb07d9330499b08afe231d4fc7c1354ea7c0a3c1c98db&w=996"
-    class="rounded-circle me-2" alt="User" /><span>+258 84 000 0000</span></a>
-</div>
-<div class="chat-item user-item"><a href="{{ route('chat.specific', 1) }}" class="chat-item-link"><img
-    src="https://img.freepik.com/free-photo/no-idea_273609-23908.jpg?t=st=1744608982~exp=1744612582~hmac=9f2c831411c907be117bb07d9330499b08afe231d4fc7c1354ea7c0a3c1c98db&w=996"
-    class="rounded-circle me-2" alt="User" /><span>Maria João</span></a>
-</div>
-<div class="chat-item user-item"><a href="{{ route('chat.specific', 1) }}" class="chat-item-link"><img
-    src="https://img.freepik.com/free-photo/no-idea_273609-23908.jpg?t=st=1744608982~exp=1744612582~hmac=9f2c831411c907be117bb07d9330499b08afe231d4fc7c1354ea7c0a3c1c98db&w=996"
-    class="rounded-circle me-2" alt="User" /><span>Carlos Silva</span></a>
-</div> --}}
-
+{{-- $chat->is_group ? $chat->name : ( ($other_side_user->username == null) ? $other_side_user->phone : $other_side_user->username ) --}}
