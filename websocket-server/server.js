7ffa6f5baa0602fc99f867e8407ef18f9656536d
Fernando Maleiane <maleianefernando3@gmail.com>
2025-05-14 16:44:43 +0200

    feat: .

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -1,28 +1,55 @@
-const Redis = require('ioredis');
+const Redis = require("ioredis");
+const SocketServer = require("socket.io");
+
+const subscriber = new Redis({
+  host: "localhost",
+  port: "6379",
+});
+
 const redis = new Redis({
-    host: 'localhost',
-    port: '6379'
+  host: "localhost",
+  port: "6379",
 });
 
-const io = require('socket.io')(3000, {
-    cors: {
-        origin: "*"
-    }
+const io = SocketServer(3000, {
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log("Connected at socket: " + socket.id)
-})
+const onlineUserEvent = "online_users";
+const messageChannel = "laravel_database_private-chat-app";
 
-redis.psubscribe("laravel_database_private-chat-app", (err, count) => {
-    console.log(count);
+io.on("connection", (socket) => {
+  console.log("Connected at socket: " + socket.id);
+
+  const userId = socket.handshake.query.user_id;
+
+  if (userId) {
+    redis.sadd(onlineUserEvent, userId);
+    emitOnlineUsers();
+
+    socket.on("disconnect", async () => {
+    //   redis.publish("user-offline", JSON.stringify({ user_id: userId }));
+      await redis.srem(onlineUserEvent, userId);
+      emitOnlineUsers();
+    });
+  }
 });
 
-redis.on('pmessage', (pattern, channel, message) => {
-    console.log(`Channel: ${channel}\n Message: ${message}`);
-    const data = JSON.parse(message)
-    // console.log(data)
-
-    io.emit(channel, data);
+subscriber.psubscribe(messageChannel, (err, count) => {
+  console.log(count);
 });
 
+subscriber.on("pmessage", (pattern, channel, message) => {
+  console.log(`Channel: ${channel}\n Message: ${message}`);
+  const data = JSON.parse(message);
+
+  io.emit(channel, data);
+});
+
+async function emitOnlineUsers() {
+  const onlineUsers = await redis.smembers(onlineUserEvent);
+  console.log(onlineUsers);
+  io.emit(onlineUserEvent, onlineUsers);
+}
